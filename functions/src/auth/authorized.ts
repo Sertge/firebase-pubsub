db330f92295081a7634f2b89c99e91d546115a45
LAPTOP-5VLIPF13\Sertge <sergiojaime@ballastlane.com>
2022-03-29 11:53:17 -0500

    first commit

---
+++ functions/src/auth/authorized.ts
@@ -0,0 +1,32 @@
+import {Request, Response, NextFunction} from "express";
+
+/**
+ *
+ * @param {Object} opts
+ * @param {Array<"admin"| "manager" | "user">} opts.hasRole approved roles
+ * @param {boolean} opts.allowSameUser toggle allow same user
+ * @return {Function} a function that returns NextFunction or an http exception
+ */
+export function isAuthorized(opts: {
+  hasRole: Array<"admin"| "manager" | "user">,
+  allowSameUser?: boolean
+}) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const {role, email, uid} = res.locals;
+    const {id} = req.params;
+
+    if (email === "admin@sertge-testing1.com") {
+      return next();
+    }
+    if (opts.allowSameUser && id && uid === id) {
+      return next();
+    }
+    if (!role) {
+      return res.status(403).send();
+    }
+    if (opts.hasRole.includes(role)) {
+      return next();
+    }
+    return res.status(403).send();
+  };
+}
