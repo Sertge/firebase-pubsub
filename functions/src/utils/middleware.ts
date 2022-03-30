3b845e19d63208d6ce5cf6500678ea0d61336fe2
LAPTOP-5VLIPF13\Sertge <sergiojaime@ballastlane.com>
2022-03-30 09:10:08 -0500

    include additional information in the message

--- functions/src/utils/middleware.ts
+++ functions/src/utils/middleware.ts
@@ -4,11 +4,16 @@ import {PubSub} from "@google-cloud/pubsub";
 
 
 const pubMessage = (req: Request, res: Response, next: NextFunction) => {
-  const TOPIC_NAME = "messages";
-  const pubsub = new PubSub();
-  const topic = pubsub.topic(TOPIC_NAME);
-  const buffer = Buffer.from(req.socket.remoteAddress);
-  topic.publish(buffer);
+  try {
+    const TOPIC_NAME = "alert-messages";
+    const pubsub = new PubSub();
+    const topic = pubsub.topic(TOPIC_NAME);
+    const buffer = Buffer.from(req.socket.remoteAddress||"127.0.0.1");
+    topic.publishMessage({data: `New request to "users" API from IP ${buffer}`});
+  } catch (err) {
+    console.log(err)
+  }
+  next();
 };
 
 export = pubMessage
