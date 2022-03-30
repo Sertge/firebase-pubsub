dfa73b60f26317830498994c4bd73aecb3f376cd
Boney <boneyaguilar@ballastalane.com>
2022-03-30 07:38:31 -0700

    use npm package for ip

--- functions/src/utils/middleware.ts
+++ functions/src/utils/middleware.ts
@@ -1,15 +1,14 @@
 import {Request, Response, NextFunction} from "express";
 
 import {PubSub} from "@google-cloud/pubsub";
-
+import {getClientIp} from "@supercharge/request-ip";
 
 const pubMessage = (req: Request, res: Response, next: NextFunction) => {
   try {
     const TOPIC_NAME = "alert-messages";
     const pubsub = new PubSub();
     const topic = pubsub.topic(TOPIC_NAME);
-    const ip = req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress || req.info.remoteAddress;
-    const buffer = Buffer.from(JSON.stringify({ip, ...res.locals}));
+    const buffer = Buffer.from(JSON.stringify({ip: getClientIp(req), ...res.locals}));
     topic.publishMessage({data: `New request to "users" API from IP ${buffer}`});
   } catch (err) {
     console.log(err);
